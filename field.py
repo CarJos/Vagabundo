e7265b1418bcd54b24d03548b702d09c3c9885bd
CarJos <carlos.sanchez.p.1992@gmail.com>
2022-02-04 13:41:47 -0500

    basic class made it

--- field.py
+++ field.py
@@ -8,10 +8,6 @@ class Field:
 
     def move_homeless(self, homeless):
         delta_x, delta_y = homeless.walk()
-
-        # path_x.append(path_x[-1] + delta_x)
-        # path_y.append(path_y[-1] + delta_y)
-
         current_coordinates = self.homeless_coordinates[homeless]
         new_coordinate = current_coordinates.move(delta_x, delta_y)
         self.homeless_coordinates[homeless] = new_coordinate
